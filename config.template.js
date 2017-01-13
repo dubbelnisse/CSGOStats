388076dd6cb2002cbc241100b388b56e7296ef25
Nils Nilsson <nilspontusnilsson@gmail.com>
2017-01-13 11:46:51 +0100

    Removed API key, so made this the initial commit

---
+++ config.template.js
@@ -0,0 +1,4 @@
+export default {
+  BASE_URL: 'http://api.steampowered.com',
+  API_KEY: '<API_KEY>'
+}
---
+++ index.ios.js
@@ -0,0 +1,5 @@
+
+import { AppRegistry } from 'react-native'
+import CSGOStats from './lib/App'
+
+AppRegistry.registerComponent('CSGOStats', () => CSGOStats)
